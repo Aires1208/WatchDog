e19f2f9da01cb2f16cd6155c4f20e634770d0bd6
Aires <aires1208@163.com>
2017-07-27 13:49:25 +0800

    Update MobileMessageSendTest.java

--- server/src/test/java/com/zte/ums/watchdog/common/sms/MobileMessageSendTest.java
+++ server/src/test/java/com/zte/ums/watchdog/common/sms/MobileMessageSendTest.java
@@ -1,12 +1,12 @@
 package com.zte.ums.watchdog.common.sms;
 
 /**
- * Created by 10183966 on 9/21/16.
+ * Created by  on 9/21/16.
  */
 public class MobileMessageSendTest {
     @org.junit.Test
     public void messageSendTest() {
         System.out.println("Test");
-//            System.out.println( MobileMessageSend.sendMsg("15800775585","告警门限"));
+//            System.out.println( MobileMessageSend.sendMsg("","告警门限"));
     }
 }
