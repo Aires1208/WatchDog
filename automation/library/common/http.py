2e525172cbf4ae98c37769914043090bc8e1bab4
Aires <aires1208@163.com>
2017-07-27 10:34:21 +0800

    Update http.py
    1

--- automation/library/common/http.py
+++ automation/library/common/http.py
@@ -1,4 +1,4 @@
-# -*- coding:utf-8 -*-
+
 import json
 import urllib2
 
@@ -22,32 +22,7 @@ class HttpClient(object):
         response = urllib2.urlopen(request)
         return response.read()
 
-    @staticmethod
-    def put(self, url, dicts):
-        request = urllib2.Request(url, json.dumps(dicts))
-        request.add_header('Content-Type', 'application/json')
-        request.get_method = lambda: 'PUT'
-        response = urllib2.urlopen(request)
-        return response.read()
-
-    @staticmethod
-    def delete(self, url, dicts):
-        request = urllib2.Request(url, json.dumps(dicts))
-        request.add_header('Content-Type', 'application/json')
-        request.get_method = lambda: 'DELETE'
-        response = urllib2.urlopen(request)
-        return response.read()
-
-
-if __name__ == '__main__':
-    counter = 0
-    while True:
-        print '--------------------------------------------------------------------'
-        print counter
-        print RESTClient('http://127.0.0.1:5000/api/v1.0/tasks').http_get()
-        print '--------------------------------------------------------------------'
-        url = 'http://127.0.0.1:5000/api/v1.0/tasks/' + str(counter)
-        dicts = {'task_name': 'qianguodong'}
+    @staticmethoong'}
         HttpClient(url).http_put(dicts)
         time.sleep(5);
         counter = counter + 1
